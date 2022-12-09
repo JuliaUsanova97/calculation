03b36ad5e454ae3a6e6627becc53e499366de242
JuliaUsanova97 <yulia.usanova@kodep.ru>
2022-12-09 14:52:21 +0300

    cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,9 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-  },
+    specPattern:
+      "**/*.{js,jsx,ts,tsx}",
+      excludeSpecPattern:
+      "cypress/support/**/*.{js,jsx,ts,tsx}"
+  }
 });
